0416d053056eec5fa7a20494f287d31768f82d54
loapits <gera59377@gmail.com>
2020-11-21 13:14:15 +0600

    project on winforms .net

---
+++ Chessboard/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Chessboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.Location = new System.Drawing.Point(353, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(142, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(348, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Первая точка";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox2.Location = new System.Drawing.Point(354, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(142, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(349, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вторая точка";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox3.Location = new System.Drawing.Point(352, 256);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(143, 26);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(348, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Результат";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(354, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(25, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 280);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawGrid);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseClick);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(572, 337);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Путь коня";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Chessboard/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace Chessboard
+{
+    public partial class Form1 : Form
+    {
+        readonly Graphics Graph;
+        readonly private NpgsqlConnection connect;
+        /*private string LETTERS = "abcdefgh";
+        private string DIGITS = "12345678";*/
+        private string LETTERS;
+        private string DIGITS;
+        private string sql;
+
+        public Form1()
+        {
+            connect = new NpgsqlConnection("Server=127.0.0.1;Port=5432;" +
+                      "User Id=postgres;Password=postgres;Database=postgres;");
+
+            InitializeComponent();
+            Graph = CreateGraphics();
+            GetChessCells();
+        }
+
+        private void GetChessCells()
+        {
+            connect.Open();
+            try
+            {
+                sql = "SELECT letters, digits FROM chessCells WHERE id = 1";
+                NpgsqlCommand comand = new NpgsqlCommand(sql, connect);
+                NpgsqlDataReader result = comand.ExecuteReader();
+
+                while (result.Read())
+                {
+                    LETTERS = result[0].ToString();
+                    DIGITS = result[1].ToString();
+                }
+                connect.Close();
+            }
+            catch (Exception e)
+            {
+                connect.Close();
+                MessageBox.Show("Error: " + e.Message);
+            }
+        }
+
+        // Inserting selected points and the result into the database
+        private async void InsertPoints(string point1, string point2, string result)
+        {
+            try
+            {
+                connect.Open();
+
+                sql = "INSERT INTO points (point1, point2, result) VALUES (@point1, @point2, @result)";
+                NpgsqlCommand comand = new NpgsqlCommand(sql, connect);
+
+                // Parameters of the request
+                comand.Parameters.AddWithValue("point1", point1);
+                comand.Parameters.AddWithValue("point2", point2);
+                comand.Parameters.AddWithValue("result", result);
+
+                await comand.ExecuteNonQueryAsync();
+                connect.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e);
+            }
+        }
+
+        // Checking of the point
+        bool Check(int mx, int my, int nx, int ny)
+        {
+            if (mx < 0 || mx > 7) return false;
+            if (my < 0 || my > 7) return false;
+            if (nx < 0 || nx > 7) return false;
+            if (ny < 0 || ny > 7) return false;
+            return (Math.Abs(mx - nx) == 2 && Math.Abs(my - ny) == 1) || 
+                (Math.Abs(mx - nx) == 1 && Math.Abs(my - ny) == 2);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int nx, ny, mx, my;
+                string str1 = textBox1.Text;
+                string str2 = textBox2.Text;
+                int[] dx = { -2, -2, -1, -1, 1, 1, 2, 2 };
+                int[] dy = { -1, 1, -2, 2, -2, 2, -1, 1 };
+
+                nx = str1[0] - 'a';
+                ny = str1[1] - '1';
+                mx = str2[0] - 'a';
+                my = str2[1] - '1';
+
+                if (Check(mx, my, nx, ny))
+                {
+                    textBox3.Text = "1";
+                }
+                else
+                {
+                    textBox3.Text = "NO";
+                }
+
+                for (int i = 0; i < 8; ++i)
+                {
+                    if (Check(mx + dx[i], my + dy[i], nx, ny))
+                    {
+                        textBox3.Text = "2";
+                    }
+                }
+
+                InsertPoints(textBox1.Text, textBox2.Text, textBox3.Text);
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Выберите точки");
+            }
+        }
+
+        // Drawing the chessboard
+        private void DrawGrid(object sender, PaintEventArgs e)
+        {
+            int i = 0;
+            // Size of the cell
+            int side = 35;
+
+            for (int k = 0; k < 64; k++)
+            {
+                // Color of the cell
+                Brush color = (k / 8 + k % 8) % 2 != 0 ? Brushes.White : Brushes.Black;
+
+                // Drawing board
+                e.Graphics.FillRectangle(color, (k / 8) * side, (k % 8) * side, side, side);
+
+                // Drawing border
+                Rectangle rect = new Rectangle(0, 0, side * 8, side * 8);
+                e.Graphics.DrawRectangle(new Pen(Color.Black), rect);
+            }
+                
+            // Location letters and digits
+            for (int n = 35; n < side * 8 + side; n += side)
+            {
+                Graph.DrawString(LETTERS[i].ToString(), new Font("Arial", 12), new SolidBrush(Color.Black), n, 5);
+                Graph.DrawString(DIGITS[i].ToString(), new Font("Arial", 12), new SolidBrush(Color.Black), 5, n);
+                i++;
+            }
+        }
+
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = LocationToCell(e.Location).ToString();
+        }
+
+        // Selecting the cell and add in the text box
+        private void Panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = LocationToCell(e.Location).ToString();
+            }
+            else
+            {
+                textBox2.Text = LocationToCell(e.Location).ToString();
+            }
+        }
+
+        // Сonverting of the point to coordinates the cell
+        string LocationToCell(Point location)
+        {
+            // A dictionary that contains coordinates where axis X
+            Dictionary<int, string> letterList = new Dictionary<int, string> 
+            { 
+                [1] = "a", [2] = "b", [3] = "c", [4] = "d", [5] = "e", [6] = "f", [7] = "g", [8] = "h" 
+            };
+            // Size of the cell where horizontal
+            int w = 35;
+            // Size of the cell where vertical
+            int h = 35;
+            // Coordinate where axis X, getting from the dictionary
+            string xLet = letterList[1 + location.X / w];
+
+            // Coordinate where axis Y
+            int yLet = 1 + location.Y / h;
+
+            // The returning string then is a point on the chessboard
+            return xLet + yLet;
+        }
+    }
+}
